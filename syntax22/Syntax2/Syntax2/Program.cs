8849ea6d79ff22e8e5c97d973a242193d6c386b9
DDon71 <theofficial1tap@gmail.com>
2022-09-18 17:57:30 -0400

    Syntax2

---
+++ syntax22/Syntax2/Syntax2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Syntax2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject in school?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("I liked math too!");
+                    break;
+
+                case "Science":
+                    Console.WriteLine("Science was alot of fun");
+                    break;
+
+                case "English":
+                    Console.WriteLine("English was a hard subject");
+                    break;
+
+                case "History":
+                    Console.WriteLine("History is my favorite subject");
+                    break;
+
+                case "Music":
+                    Console.WriteLine("I hated music");
+                    break;
+
+                default:
+                    Console.WriteLine("Interesting.");
+                    break;
+            }
+
+        }
+    }
+}
+
